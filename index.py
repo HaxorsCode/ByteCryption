d1e993028f6b9097257bde20173294195342ab3f
HanzHaxors <hanzhaxors@gmail.com>
2021-02-07 09:07:25 +0700

    Add file input and new '-ne' flag

--- index.py
+++ index.py
@@ -23,19 +23,37 @@ def decrypt(encrypted: str, key: str='hanz'):
 	return stage_1
 
 if __name__ == '__main__':
-	import sys
+	import sys, os
 	argv = sys.argv[1:]
 	key = 'hanz'
+	no_escape = False
+
+	decrypted = ''
+	encrypted = ''
 
 	try:
 		key = argv[1]
+		no_escape = '-ne' in argv
 	except IndexError:
 		pass
 
-	if argv[0].startswith('e6'):
-		encrypted = argv[0][2:]
-		decrypted = decrypt(encrypted, key=key)
-		print(decrypted.encode('unicode_escape').decode())
+	if os.path.isfile(argv[0]):
+		with open(argv[0], "rb") as f:
+			file_content = f.read().decode()
+
+			if file_content.startswith('e6'):
+				_encrypted = file_content[2:]
+				decrypted = decrypt(_encrypted, key=key)
+			else:
+				encrypted = encrypt(file_content, key=key)
 	else:
-		encrypted = encrypt(argv[0], key=key)
-		print(encrypted.encode('unicode_escape').decode())
+		if argv[0].startswith('e6'):
+			_encrypted = argv[0][2:]
+			decrypted = decrypt(_encrypted, key=key)
+		else:
+			encrypted = encrypt(argv[0], key=key)
+
+	if no_escape:
+		print((encrypted or decrypted))
+	else:
+		print((encrypted or decrypted).encode('unicode_escape').decode())
